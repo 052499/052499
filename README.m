87ea3acdcf253dbefed58405dde844e8765a0fd0
052499 <143764266+052499@users.noreply.github.com>
2023-09-06 10:57:46 -0500

    Khalida

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m Khalida 
+- 👀 I’m interested in cooking, trekking and travelling
+- 🌱 I’m currently learning how to collaborate with new people in new country
+- 💞️ I’m looking to collaborate with my team mates.
+- 📫 mohammadkhalidaparveen23@gmail.com
+
+<!---
+Khalida is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
